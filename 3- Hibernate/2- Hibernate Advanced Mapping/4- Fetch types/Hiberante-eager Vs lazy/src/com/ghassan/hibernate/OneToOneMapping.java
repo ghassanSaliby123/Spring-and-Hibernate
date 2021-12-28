02830374b013ebacd5169f91537b884f03893837
ghassan saliby <ghassan.saliby@gmail.com>
2021-12-28 09:43:07 +0100

    Update

---
+++ 3- Hibernate/2- Hibernate Advanced Mapping/4- Fetch types/Hiberante-eager Vs lazy/src/com/ghassan/hibernate/OneToOneMapping.java
@@ -0,0 +1,65 @@
+package com.ghassan.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
+
+import com.ghassan.hibernate.entity.Instructor;
+import com.ghassan.hibernate.entity.InstructorDetail;
+
+
+public class OneToOneMapping {
+
+	public static void main(String[] args) {
+		
+		//Create sessionFactory 
+		SessionFactory factory =  new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		//Create session
+		Session session  = factory.getCurrentSession();
+		
+		try {
+			
+			//create the objects
+			System.out.println("1- Create Instructor and InstructorDetail");
+			Instructor instructor = 
+					new Instructor("Ghassan","Saliby","Ghassan@gmail.com");
+			
+			InstructorDetail instructorDetail = 
+					new InstructorDetail("Ghassan Channel","Sports");
+			
+			//associate the objects 
+			//Here we associate the Instructor with the InstructorDetail
+			System.out.println("2- Associate the objects");
+			instructorDetail.setInstructor(instructor);
+			
+			//start a transaction
+			System.out.println("3- start a transaction");
+			session.beginTransaction();
+			
+			//save the instructorDetail
+			//When we save the InstructorDetail, the Instructor will be saved
+			//because we define the CascadeType.ALL and the relationship is 
+			//Bidirectional
+			System.out.println("4- Saving Instructor and InstructorDetail");
+			System.out.println("Instructor: " + instructor);
+			System.out.println("InstructorDetail: " + instructorDetail);
+			session.save(instructorDetail);
+			
+			//commit the transaction
+			System.out.println("5- commit the transaction");
+			session.getTransaction().commit();	
+			
+			System.out.println("Done!");
+		}
+		finally {
+			System.out.println("Closing the factory...");
+			factory.close();
+		}
+	}
+
+}
