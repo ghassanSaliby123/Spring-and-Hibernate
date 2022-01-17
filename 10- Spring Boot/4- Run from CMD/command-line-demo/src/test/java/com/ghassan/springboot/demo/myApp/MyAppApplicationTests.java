3608df127581c681bb41694caa30bfc15fac299d
ghassan saliby <ghassan.saliby@gmail.com>
2022-01-17 11:07:16 +0100

    Update

---
+++ 10- Spring Boot/4- Run from CMD/command-line-demo/src/test/java/com/ghassan/springboot/demo/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghassan.springboot.demo.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
